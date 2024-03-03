4dfc82e61e6cd994c3aab054e56078de3a709fd0
Drake Davis <drakedavis0203@gmail.com>
2024-03-03 18:11:11 -0500

    New sentence randomization script for text analysis using the NLTK library to tokenize large texts into sentences, and then randomly selects a subset of these sentences for creative processing. The script leverages the Punkt tokenizer for sentence segmentation

--- Sentence_Generator.py
+++ Sentence_Generator.py
@@ -1,2 +1,31 @@
+import nltk
+import pandas as pd
+import random
 
+# Download punkt
+nltk.download('punkt')
 
+def tokenize_text_to_sentences(text):
+    """Tokenize a given text into sentences"""
+    return nltk.sent_tokenize(text)
+
+def load_texts(filenames):
+    """Load texts from files and tokenize into sentences"""
+    sentences = []
+    for filename in filenames:
+        with open(filename, 'r', encodings='utf-8') as file:
+            text = file.read()
+            sentences.extend(tokenize_text_to_sentences(text))
+    return sentences
+
+def randomize_sentences(sentences, n=10):
+    """Randomly select n sentences from the list"""
+    return random.sample(sentences, n)
+
+if __name__ == "__main":
+    filenames = ['path/to/text1.txt', 'path/to/text2.txt']
+    sentences = load_texts(filenames)
+    randomized_sentences = randomize_sentences(sentences, 10)
+
+    for sentence in randomized_sentences:
+        print(sentence)
