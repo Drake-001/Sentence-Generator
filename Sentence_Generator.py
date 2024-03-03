514f91ed31bc70b5e612743d2d15057fce918e87
Drake Davis <drakedavis0203@gmail.com>
2024-03-03 17:52:55 -0500

    Starting over

--- Sentence_Generator.py
+++ Sentence_Generator.py
@@ -1,9 +1,2 @@
-def sentenceInput():
-    passage1 = input("Enter Passage here: ")
-    splitPassage1 = passage1.split(". ")
-    storePassage = []
 
-    for i in splitPassage1:
-        storePassage.append(i)
-    print(storePassage)
 
