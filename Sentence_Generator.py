123e369adbbc0ccd456a5e5ca5925d5ca4ac3af2
Drake Davis <drakedavis0203@gmail.com>
2024-03-17 18:30:53 -0400

    Added tags that correspond to each file, so any output sentence will be identifiable by which source it came from

--- Sentence_Generator.py
+++ Sentence_Generator.py
@@ -12,10 +12,12 @@ def tokenize_text_to_sentences(text):
 def load_texts(filenames):
     """Load texts from files and tokenize into sentences"""
     sentences = []
-    for filename in filenames:
+    tags = [" -I", " -Q", " -U", " -G"]  # Corresponding tags for each file
+    for filename, tag in zip(filenames, tags):
         with open(filename, 'r', encoding='utf-8') as file:
             text = file.read()
-            sentences.extend(tokenize_text_to_sentences(text))
+            tokenized_sentences = tokenize_text_to_sentences(text)
+            sentences.extend([sentence + tag for sentence in tokenized_sentences])
     return sentences
 
 def randomize_sentences(sentences, n=20):
@@ -25,7 +27,7 @@ def randomize_sentences(sentences, n=20):
 
 
 if __name__ == "__main__":
-    filenames = ["C:/Users/ddavi/Python projects/Sentence_Gen/Inferno.txt", "C:/Users/ddavi/Python projects/Sentence_Gen/Ulysses.txt", "C:/Users/ddavi/Python projects/Sentence_Gen/Gravity.txt"]
+    filenames = ["C:/Users/ddavi/Python projects/Sentence_Gen/Inferno.txt", "C:/Users/ddavi/Python projects/Sentence_Gen/Quixote.txt", "C:/Users/ddavi/Python projects/Sentence_Gen/Ulysses.txt", "C:/Users/ddavi/Python projects/Sentence_Gen/Gravity.txt"]
     sentences = load_texts(filenames)
     randomized_sentences = randomize_sentences(sentences, 10)
     for sentence in randomized_sentences:
